63fd9163871d9dcfc1b99c0e7bd67abb402aaaeb
Gazedo <hconnor3@gmail.com>
2015-04-17 21:32:32 -0700

    updated with hall effect rotation calculations

--- extra/motor_control.ino
+++ extra/motor_control.ino
@@ -1,26 +1,37 @@
 #include <PID_v1.h>
+#define int2 2
+#define int3 3
+#define int4 4
 
 int MotorA = 6;
 int MotorB = 9;
 int MotorC = 10;
 int val = 0;
+//Hall effect sensor variables
+int halla_old = 0, hallb_old = 0, hallc_old = 0;
+double rota = 0, rotb = 0, rotc =0;
+int timeold = 0;
+volatile int halla = 0, hallb = 0, hallc = 0;
+volatile int ina, inb, inc;
+//PID variables
 char motorSelect = 0;
-
 double Setpointa, Inputa, Outputa;
 double Setpointb, Inputb, Outputb;
 double Setpointc, Inputc, Outputc;
-
-volatile double veca, vecb, vecc;
-
 double Kp = 1, Kd = 0.25, Ki = .05;
-PID aPID(&Inputa, &Outputa, &Setpointa, Kp, Ki, Kd, DIRECT);
-PID bPID(&Inputb, &Outputb, &Setpointb, Kp, Ki, Kd, DIRECT);
-PID cPID(&Inputc, &Outputc, &Setpointc, Kp, Ki, Kd, DIRECT);
+PID aPID(&Inputa, &Outputa, &Setpointa, Kp, Kd, Ki, DIRECT);
+PID bPID(&Inputb, &Outputb, &Setpointb, Kp, Kd, Ki, DIRECT);
+PID cPID(&Inputc, &Outputc, &Setpointc, Kp, Kd, Ki, DIRECT);
 
 void setup(){
+	//Pinmodes for the motor's PWM
 	pinMode(MotorA,OUTPUT);
 	pinMode(MotorB,OUTPUT);
 	pinMode(MotorC,OUTPUT);
+	//Defining the interupt pins as interrupt, required by the Teensy 3.1
+	pinMode(int2, INPUT);
+	pinMode(int3, INPUT);
+	pinMode(int4, INPUT);
 
 	Serial.begin(9600);
 
@@ -32,7 +43,10 @@ void setup(){
 	bPID.SetMode(AUTOMATIC);
 	cPID.SetMode(AUTOMATIC);
 
-	attachInterrupt(, function, mode);
+	attachInterrupt(int2, hall_isra, RISING);
+	attachInterrupt(int3, hall_isrb, RISING);
+	attachInterrupt(int4, hall_isrc, RISING);
+
 }
 
 void loop(){
@@ -48,10 +62,6 @@ void loop(){
     	//Serial.write("Got ");Serial.println(val,DEC);
     	Serial.flush(); // get rid of excess
   	}
-
-  	Inputa = veca;
-	Inputb = vecb;
-	Inputc = vecc;
 
   	switch (motorSelect) {
   		case 'a':
@@ -69,8 +79,35 @@ void loop(){
 	aPID.Compute();
 	bPID.Compute();
 	cPID.Compute();
+
+	if( millis() - timeold > 20){
+		rota = (halla - halla_old)/(millis()-timeold);
+		rotb = (hallb - hallb_old)/(millis()-timeold);
+		rotc = (hallc - hallc_old)/(millis()-timeold);
+		halla_old = halla;
+		hallb_old = hallb;
+		hallc_old = hallc;
+	}
+
+  	Inputa = rota;
+	Inputb = rotb;
+	Inputc = rotc;
 }
 
-void hall_isr(){
+void hall_isra(){
+	if (ina > (millis()+1)){
+	halla++;
+}
+}
 
+void hall_isrb(){
+	if (inb > (millis()+1)){
+	hallb++;
+}
+}
+
+void hall_isrc(){
+	if (inc > (millis()+1)){
+	hallc++;
+}
 }
